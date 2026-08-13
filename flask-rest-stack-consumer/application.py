a9f4855c2153a9d03823a0ff8bb3cf84851fdb9a
shavkatjon <shavkatjon.yuldashev.0411@gmail.com>
2026-08-13 20:44:22 +0900

    Add Drink model and modify index route response
    Added Drink model for database and updated index response.

--- flask-rest-stack-consumer/application.py
+++ flask-rest-stack-consumer/application.py
@@ -6,6 +6,15 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///drinks.db'
 db = SQLAlchemy(app)
 
 
+class Drink(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    description = db.Column(db.String(120))
+
+    def __repr__(self):
+        return f"{self.name} - {self.description}"
+
+
 @app.route('/')
 def index():
-    return "Hello, World!"
+    return "Hello, brajer"
